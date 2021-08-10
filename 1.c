e352ec1ba7ce4b36e34839f2726017e03b43c713
Thiago.Junkes <thiago.junkes3@gmail.com>
2021-08-09 21:47:28 -0300

    Nome Variaveis

--- 1.c
+++ 1.c
@@ -7,21 +7,21 @@ typedef struct {
 		int celular;
 		char email[36];
 		int codigo;
-} Contato;
+} Lista;
 
-int listar(Contato c[], int cont){
+int listar(Lista contato[], int cont){
     printf(" -=!CONTATOS!=- \n");
     for (int i = 0; i < cont; i++){
-        printf("Codigo de Identificacao: %d\n", c[i].codigo);
-        printf("Nome: %s\n", c[i].nome);
-        printf("Celular: %i\n", c[i].celular);
-        printf("Email: %s\n", c[i].email);
+        printf("Codigo de Identificacao: %d\n", contato[i].codigo);
+        printf("Nome: %s\n", contato[i].nome);
+        printf("Celular: %i\n", contato[i].celular);
+        printf("Email: %s\n", contato[i].email);
         printf("-===========-\n");
     }
 }
 
 int main() {
-	Contato c[1000];
+	Lista contato[1000];
     int x=1, opcao, cont=0;
     while(x>=1){
 		printf("!AGENDA DE CONTATOS!\n");
@@ -33,27 +33,27 @@ int main() {
 		    case 1:   //Criar novos Contatos
                 printf(" -=!REGISTRAR CONTATO!=- \n");
                 printf("Digite o nome: ");
-                scanf("%s", &c[cont].nome);
+                scanf("%s", &contato[cont].nome);
                 fflush(stdin);
                 printf("Digite o numero: ");
-                scanf("%i", &c[cont].celular);
+                scanf("%i", &contato[cont].celular);
                 printf("Digite o email: ");
-                scanf("%s", &c[cont].email);
+                scanf("%s", &contato[cont].email);
                 fflush(stdin);
-                c[cont].codigo = cont + 1;
+                contato[cont].codigo = cont + 1;
                 cont++;
                 break;
             case 2:   //Excluir Contatos
                 printf(" -=!EXCLUIR CONTATO!=- \n");
-                listar(c, cont);
+                listar(contato, cont);
                 break;
             case 3:  //Alterar Contatos
                 printf(" -=!ALTERAR CONTATO EXISTENTE!=- \n");
-                listar(c, cont); //printa os contatos com numero na frente
+                listar(contato, cont); //printa os contatos com numero na frente
                 break;
             case 4:   //Listar contatos
                 printf(" -=!LISTAR CONTATOS!=- \n");
-                listar(c, cont);
+                listar(contato, cont);
                 break;
             case 5:    //Fecha o Programa
                 printf("FECHANDO PROGRAMA...\n");
