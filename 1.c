b0efc0d42b72c1f3cc1181bc1a38313e7832f834
Pedro Henrique do Rosário <rosario180912@gmail.com>
2021-08-11 22:12:58 -0300

    Identação do código

--- 1.c
+++ 1.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
-		char nome[30];
-		int celular;
-		char email[36];
-		int codigo;
+typedef struct
+{
+    char nome[30];
+    int celular;
+    char email[36];
+    int codigo;
 } Lista;
 
-int listar(Lista contato[], int cont){
+//Função que válida se o código de usuário
+int validaCodigo(int codigo)
+{
+    while (codigo < 1 || codigo >= cont + 1)
+    { //valida se o codigo existe
+        printf("Codigo inexistente, tente novamente: ");
+        scanf("%i", &alterar);
+    }
+}
+
+int listar(Lista contato[], int cont)
+{
     printf(" -=!CONTATOS!=- \n");
-    for (int i = 0; i < cont; i++){
-        if(contato[i].codigo>0){
+    for (int i = 0; i < cont; i++)
+    {
+        if (contato[i].codigo > 0)
+        {
             printf("Codigo de Identificacao: %d\n", contato[i].codigo);
             printf("Nome: %s\n", contato[i].nome);
             printf("Celular: %i\n", contato[i].celular);
@@ -23,59 +37,67 @@ int listar(Lista contato[], int cont){
 //Criar uma variavel de validação de contato, assim só podera excluir, alterar ou procurar algum contato caso essa
 //variavel for 1. No inicio do codigo a variavel é 0 e só é setada para 1 na criação de um contato
 //Possivel problema, uma variavel a mais a se guardar no arquivo
-int main() {
-	Lista contato[1000];
-    int x=1, opcao, cont=0;
-    while(x>=1){
-		printf("!AGENDA DE CONTATOS!\n");
-		printf("Escolha uma opcao:\n1--Registrar novo contato\n2--Excluir contato existente\n");
+int main()
+{
+    Lista contato[1000];
+    int x = 1, opcao, cont = 0;
+    while (x >= 1)
+    {
+        printf("!AGENDA DE CONTATOS!\n");
+        printf("Escolha uma opcao:\n1--Registrar novo contato\n2--Excluir contato existente\n");
         printf("3--Alterar contator existente\n4--Listar contatos existentes\n5--Fechar Programa\n");
-		scanf("%i", &opcao);
-		printf("-==================-\n");
-		switch (opcao){
-		    case 1:   //Criar novos Contatos
-                printf(" -=!REGISTRAR CONTATO!=- \n");
-                printf("Digite o nome: ");
-                scanf("%s", &contato[cont].nome);
-                fflush(stdin);
-                printf("Digite o numero: ");
-                scanf("%i", &contato[cont].celular);
-                printf("Digite o email: ");
-                scanf("%s", &contato[cont].email);
-                fflush(stdin);
-                //Precisa validar o email
-                contato[cont].codigo = cont + 1;
-                cont++;
-                break;
-            case 2:   //Excluir Contatos
-                printf(" -=!EXCLUIR CONTATO!=- \n");
-                listar(contato, cont);
-                int excluir;
-                printf("Escolha o codigo do contato que deseja excluir: ");
-                scanf("%i", &excluir);
-                while(excluir < 1 || excluir >= cont+1){ //valida se o codigo existe
-                    printf("Codigo inexistente, tente novamente: ");
-                    scanf("%i", &excluir);
-                }
-                contato[excluir-1].codigo= contato[excluir-1].codigo * -1;
-                break;
-            case 3:  //Alterar Contatos
-                printf(" -=!ALTERAR CONTATO EXISTENTE!=- \n");
-                listar(contato, cont); //printa os contatos com numero na frente
-                break;
-            case 4:   //Listar contatos
-                printf(" -=!LISTAR CONTATOS!=- \n");
-                listar(contato, cont);
-                break;
-            case 5:    //Fecha o Programa
-                printf("FECHANDO PROGRAMA...\n");
-                //Salvar Dados e entao fecha
-                x=0;
-                break;
-            default:   //Não escolheu um numero entre 1 a 5
-                printf("Opcao invalida, tente novamente!\n");
-                break;
-            }
-	}
-	return 0;
+        scanf("%i", &opcao);
+        printf("-==================-\n");
+        switch (opcao)
+        {
+        case 1: //Criar novos Contatos
+            printf(" -=!REGISTRAR CONTATO!=- \n");
+            printf("Digite o nome: ");
+            scanf("%s", &contato[cont].nome);
+            fflush(stdin);
+            printf("Digite o numero: ");
+            scanf("%i", &contato[cont].celular);
+            printf("Digite o email: ");
+            scanf("%s", &contato[cont].email);
+            fflush(stdin);
+            //Precisa validar o email
+            contato[cont].codigo = cont + 1;
+            cont++;
+            break;
+        case 2: //Excluir Contatos
+            printf(" -=!EXCLUIR CONTATO!=- \n");
+            listar(contato, cont);
+            int excluir;
+            printf("Escolha o codigo do contato que deseja excluir: ");
+            scanf("%i", &excluir);
+            validaCodigo(excluir);
+
+            contato[excluir - 1].codigo = contato[excluir - 1].codigo * -1;
+            break;
+        case 3: //Alterar Contatos
+            printf(" -=!ALTERAR CONTATO EXISTENTE!=- \n");
+            listar(contato, cont); //printa os contatos com numero na frente
+            int alterar;
+            printf("Escolha o codigo do contato que deseja alterar:");
+            printf("%i", &alterar);
+            validaCodigo(alterar);
+
+            printf("")
+
+            break;
+        case 4: //Listar contatos
+            printf(" -=!LISTAR CONTATOS!=- \n");
+            listar(contato, cont);
+            break;
+        case 5: //Fecha o Programa
+            printf("FECHANDO PROGRAMA...\n");
+            //Salvar Dados e entao fecha
+            x = 0;
+            break;
+        default: //Não escolheu um numero entre 1 a 5
+            printf("Opcao invalida, tente novamente!\n");
+            break;
+        }
+    }
+    return 0;
 }
